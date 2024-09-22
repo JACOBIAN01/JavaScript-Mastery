ed101ec8a24129005b8821c14f1833e49e228629
JACOBIAN01 <subhadeepghorai23@gmail.com>
2024-09-23 00:28:54 +0530

    To-Do List

---
+++ To-Do-List/script.js
@@ -0,0 +1,17 @@
+document.getElementById("AddTaskButton").addEventListener('click',addTask);
+
+function addTask(){
+    const taskInput = document.getElementById("taskinput");
+    const taskText = taskInput.value.trim()
+
+    if(taskText){
+        const taskList = document.getElementById("taskList");
+        const li = document.createElement('li');
+        li.textContent = taskText;
+
+        taskList.appendChild(li);
+        taskInput.value = "";
+    }
+
+
+}
