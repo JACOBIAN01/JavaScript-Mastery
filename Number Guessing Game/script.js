2e19c1c78c2651f3b9e104c2f845f1ef41694996
JACOBIAN01 <subhadeepghorai23@gmail.com>
2024-09-24 21:50:03 +0530

    Update

--- Number Guessing Game/script.js
+++ Number Guessing Game/script.js
@@ -1,21 +1,27 @@
-const randomNumber = Math.floor(Math.random()*100);
+document.getElementById('button').addEventListener('click',Show)
+document.getElementById("help").addEventListener("click", help);
+const randomNumber = Math.floor(Math.random() * 100);
 
-
-document.getElementById('button').addEventListener('click',Show())
+function help(){
+  const res = document.getElementById("result");
+  const newText = document.createElement('p');
+  newText.textContent=`Answer is ${randomNumber}`;
+  res.appendChild(newText);
+}
 
 function Show(){
-const numberInput = document.getElementById("numberInput");
-const My_number = numberInput.value;
-document.getElementById("");
-if (randomNumber == My_number) {
-  const res = document.getElementById("res");
-  document.getElementById("res").innerHTML = `
-Correct ANswer!
-`;
-} else {
-  document.getElementById("res").innerHTML = `<h4>
-Correct ANswer!</h4>
-`;
+const res = document.getElementById("result");
+res.innerHTML="";
+const numberInput = document.getElementById("numberInput").value;
+if (randomNumber == numberInput) {
+ const newText = document.createElement('p');
+ newText.textContent="Correct Guess!";
+ res.appendChild(newText);
+}
+if (randomNumber != numberInput) {
+  const newText = document.createElement("p");
+  newText.textContent = "Wrong Guess!";
+  res.appendChild(newText);
 }
 }
 
