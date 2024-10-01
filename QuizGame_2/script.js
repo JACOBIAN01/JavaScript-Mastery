d9f467216162a7e2c941eedbaea4708bc26d6cb0
JACOBIAN01 <subhadeepghorai23@gmail.com>
2024-10-01 13:44:24 +0530

    Update

---
+++ QuizGame_2/script.js
@@ -0,0 +1,58 @@
+const quizData = [
+  {
+    question: "What is the Capital of France ?",
+    answer: "Madrid",
+  },
+  {
+    question: "Who wrote 'Hamlet'?",
+    answer: "J.K. Rowling",
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    answer: "Earth"
+  },
+
+];
+
+
+const QuestionElement = document.getElementById('question');
+const AnswerElement = document.getElementById('answer');
+document.getElementById('Next').addEventListener('click',NextQ);
+
+let currentQ = 0;
+
+
+function loadQuestion() {
+  const Question = quizData[currentQ];
+  QuestionElement.textContent = Question.question;
+}
+
+
+function NextQ(){
+    if(quizData.length<=currentQ){
+        alert("Quiz Complete");
+        return;
+    }
+
+
+   const correct = quizData[currentQ].answer.trim().toLowerCase();
+   const userAnswer = AnswerElement.value.trim().toLowerCase();
+
+
+    if (Answer==correct){
+        alert("Correct Answer !");
+        currentQ++;
+        
+    }else{
+        alert("Try Again");
+    }
+
+    AnswerElement.value='';
+    if (currentQ < quizData.length) {
+     loadQuestion();
+    }else{
+        alert("Quiz Complete!")
+    }
+}
+
+loadQuestion()
