2e9fb7a128b287a6a2ed853c7cb89bc867c6fc06
JACOBIAN01 <subhadeepghorai23@gmail.com>
2024-10-02 03:50:08 +0530

    update

--- Generate Random Password/script.js
+++ Generate Random Password/script.js
@@ -4,15 +4,21 @@ const Result = document.getElementById("password");
 
 
 function GeneratePassword(){
+    Result.innerHTML='';
     const Number = Math.floor(Math.random()*100);
-    let i =4;
-    let text = '';
-    for(i=0;i<4;i++){
-        text =text+characters[Math.floor(Math.random() * characters.length)]; 
+    let n = document.getElementById('length').value;
+    if(n>=5){     
+let text = "";
+for (i = 0; i < n - 2; i++) {
+  text = text + characters[Math.floor(Math.random() * characters.length)];
+}
+const password = text + String(Number);
+const res = document.createElement("p");
+res.textContent = `Your Password is => ${password}`;
+Result.appendChild(res);
+    }else{
+alert("Size must be equal or grater than 5")
     }
-    const password =text+String(Number);
-    const res = document.createElement('p')
-    res.textContent = `Your Password is ${password}`;
-    Result.appendChild(res);
+    
 }
 
