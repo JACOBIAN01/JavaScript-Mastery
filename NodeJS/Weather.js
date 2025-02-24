25cce237ec24cef16150faba03d19ea1f83be4ec
JACOBIAN01 <subhadeepghorai23@gmail.com>
2025-02-24 18:53:57 +0530

    Update

--- NodeJS/Weather.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-
-http.createServer(function(req,res){
-    var q = url.parse(req.url,true);
-    var filename = "."+q.pathname;
-})
